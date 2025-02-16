354c41f1e98e4b74c84b79a80d1db973b5640e94
Yuen Ler Chow <lerchow@gmail.com>
2025-02-16 01:09:49 -0800

    title change

--- visualizer/src/components/NavBar.js
+++ visualizer/src/components/NavBar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          PulseAI
+          PulseAI Database and Log Viewer
         </Typography>
         <Button color="inherit" component={Link} to="/">
           Database
